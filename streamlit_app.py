ac2c0bc9f5d2187c81b6b136817f0a6ba7f0020d
harik20 <jharikrishnan1993@gmail.com>
2023-09-03 23:52:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega & Peas')
+streamlit.text('Kale, Spinach & Rocket Smootie')
+streamlit.text('Hard boiled Free-range Egg')
